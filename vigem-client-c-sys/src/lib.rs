05bcb1ca986abece115a8bc0aed0ca7dcb228d79
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-08-28 15:37:45 +0200

    tests: remove useless warnings coming from bindgen's code

--- vigem-client-c-sys/src/lib.rs
+++ vigem-client-c-sys/src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![cfg_attr(test, allow(deref_nullptr, unaligned_references))]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
